5f69aba41ab2f5839194e6b59840d6d457166b9c
NioDeBacker <nio.debacker@student.hogent.be>
2023-04-01 22:40:51 +0200

    some more functions

--- huffman_encoding.py
+++ huffman_encoding.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-import sys
-
-input_path = sys.argv[0]
-
 class Node:
     def __init__(self, prob, symbol, left=None, right=None):
         
@@ -39,19 +35,65 @@ def calculate_probability(data):
 
 # a list of nodes merged together untill there is a tree, returns the root node
 def create_tree(nodes):
-    sorted_nodes = sorted(nodes)
-    while len(sorted_nodes > 1):
+    while len(nodes > 1):
+        nodes = sorted(nodes, key=lambda x: x.prob)
         # get two nodes with lowest probability
-        min_node_1 = sorted_nodes[0]
-        min_node_2 = sorted_nodes[1]
+        right_node = nodes[0]
+        left_node = nodes[1]
 
-        root_node = Node(min_node_1.prob + min_node_2.prob, '', min_node_1, min_node_2)
+        right_node.code = 1
+        left_node.code = 0
 
-        sorted_nodes.remove(min_node_1)
-        sorted_nodes.remove(min_node_2)
-        sorted_nodes.append(root_node)
-        sorted_nodes = sorted(sorted_nodes)
-    return sorted_nodes[0]
+        # merge them under one parent node
+        parent_node = Node(right_node.prob + left_node.prob, '', right_node, left_node)
 
-def write_encoded(data, output_path):
-    print("Writing to file...") 
+        nodes.remove(right_node)
+        nodes.remove(left_node)
+        nodes.append(parent_node)
+    return nodes[0]
+
+
+def calculate_code(node, value=''):
+    new_value = value + str(node.code)
+    
+    if(node.left):
+        calculate_code(node.left, new_value)
+    if(node.right):
+        calculate_code(node.right, new_value)
+
+    if(not node.left and not node.right):
+        codes[node.symbol] = new_value
+    
+    return codes
+def bitstring_to_bytes(s):
+    return int(s, 2).to_bytes((len(s) + 7) // 8, byteorder='big')
+
+def encode_file(data, codes, output_path):
+    bit_string = ""
+    for d in data:
+        bit_string += codes[d]
+    
+    bytes_array = bitstring_to_bytes(bit_string)
+
+    with open(output_path, 'wb') as file_obj:
+        file_obj.write(bytes_array)
+
+
+import sys
+
+input_path = sys.argv[0]
+
+output_path = sys.argv[0]
+
+codes = dict()
+
+with open(input_path, 'r') as file:
+    data = file.read()
+
+probabilities = calculate_probability(data)
+
+# create an array with nodes
+
+# calculate codes
+
+# encode file
